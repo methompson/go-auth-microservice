b2c1456b0e49242a324e6f19d656244e4e9f09cc
Mathew Thompson <mathewthompson@Mathews-MacBook-Pro.local>
2021-07-02 10:14:53 -0500

    Added echo statement to script

--- gen-rsa-key.sh
+++ gen-rsa-key.sh
@@ -1,5 +1,5 @@
+echo "Don't Add a passphrase"
 ssh-keygen -t rsa -b 4096 -m PEM -f jwtRS256.key
-# Don't add passphrase
 openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
 cat jwtRS256.key
 cat jwtRS256.key.pub
